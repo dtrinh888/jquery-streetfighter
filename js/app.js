ee8001376509f664b4d6b5cec3b4867545f2d53d
dtrinh888 <dtrinh888@gmail.com>
2015-09-13 19:28:00 -0700

    added cool stance

--- js/app.js
+++ js/app.js
@@ -15,16 +15,30 @@ $(document).ready(function(){
 				{'left': '1020px'},
 				500,
 				function() {
+					$(this).stop();
 					$(this).hide();
 					$(this).css('left', '520px');
 				}
 			);
 		
 	})
+
 	.mouseup(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+
+$(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-action').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
 });
 
 function playHadouken () {
@@ -32,3 +46,4 @@ function playHadouken () {
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 }
+	
